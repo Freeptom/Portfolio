851775cf36cb6749d98ed0de2f640bbefd4000eb
Freeptom <32514405+Freeptom@users.noreply.github.com>
2018-04-04 22:03:38 +0100

    added js button

---
+++ index.js
@@ -0,0 +1,16 @@
+//remove class 'read-more which changes overflow from hidden to auto'
+
+//get all elements with 'content' class
+const allContent = document.getElementsByClassName('content');
+
+//get all elements with 'read-more__button' class
+const readMoreButtons = document.getElementsByClassName('read-more__button')
+
+//convert from HTMLCollection to Array. For each button remove '.read-more' class
+Array.from(readMoreButtons).forEach(function(button, i) {
+  button.addEventListener('click', function() {
+    //remove read-more class from first element
+    allContent[i].classList.remove('read-more');
+    button.classList.add('hidden');
+  });
+});
